5577781c5d686a7123482209151bbeb5703b9860
ronuryagmur <ronuryagmur@gmail.com>
2024-04-17 23:08:47 +0300

    retrofit url changed

--- app/src/main/java/com/onur/diplayground/RepositoryProvider.kt
+++ app/src/main/java/com/onur/diplayground/RepositoryProvider.kt
@@ -3,7 +3,7 @@ package com.onur.diplayground
 import retrofit2.Retrofit
 
 class RepositoryProvider {
-    private val retrofit = Retrofit.Builder().baseUrl("api.service.com").build()
+    private val retrofit = Retrofit.Builder().baseUrl("https://www.google.com").build()
     private val userDataFromLocal = UserDataFromLocal()
     private val userDataFromRemote = UserDataFromRemote(retrofit)
     val repository = Repository(userDataFromLocal, userDataFromRemote)
