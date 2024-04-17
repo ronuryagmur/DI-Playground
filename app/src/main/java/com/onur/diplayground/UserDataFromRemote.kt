5ba7b9de5ea7a181d893e7b84c570c02bd1a86c8
ronuryagmur <ronuryagmur@gmail.com>
2024-04-17 23:05:14 +0300

    test commit

--- app/src/main/java/com/onur/diplayground/UserDataFromRemote.kt
+++ app/src/main/java/com/onur/diplayground/UserDataFromRemote.kt
@@ -5,6 +5,6 @@ import retrofit2.Retrofit
 class UserDataFromRemote(private val retrofit: Retrofit) {
     fun getData(): String {
         retrofit.callFactory()
-        return "HELLOOOOO"
+        return "HELLOOOOOO"
     }
 }
